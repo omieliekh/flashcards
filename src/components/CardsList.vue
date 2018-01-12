d315d6fc49f832768d07c6eff5738295870279dc
Oleksandr Mieliekh <meleh.alexandr@gmail.com>
2018-01-12 22:24:18 +0800

    Refactor Cards into LinkCard

--- src/components/CardsList.vue
+++ src/components/CardsList.vue
@@ -3,32 +3,29 @@
     <h1>{{ msg }}</h1>
 
     <b-row>
-      <b-col sm="6" md="4" lg="3" v-for="slideshow in slideshows"  :key="slideshow.id">
-        <router-link :to="'/slideshow/' + slideshow.id">
-          <b-card
-            overlay
-            :img-src="getImagePath() + slideshow.image_path"
-            :img-alt="slideshow.name"
-            text-variant="white"
-            :title="slideshow.name"
-            class="mb-4"
-          ></b-card>
-        </router-link>
-      </b-col>
+      <link-card
+        v-for="slideshow in slideshows"  :key="slideshow.id"
+        :link="'/slideshow/' + slideshow.id"
+        :image-path="getImagePath() + slideshow.image_path"
+        :name="slideshow.name"
+      ></link-card>
     </b-row>
   </div>
 </template>
 
 <script>
   import Auth from '../services/Auth'
+  import LinkCard from '@/components/LinkCard'
 
   export default {
     name: 'cards',
+    components: {
+      LinkCard
+    },
     data () {
       return {
         msg: 'Cards List',
         slideshows: [],
-
         getImagePath: Auth.getImagePath.bind(Auth)
       }
     },
@@ -42,12 +39,6 @@
             this.slideshows = slideshows
           })
           .catch(console.error)
-
-        this.$http.get('/api/slideshows/1')
-          .then(slideshow => {
-            console.log('slideshow id:1: : ', slideshow)
-          })
-          .catch(console.error)
       }
     }
   }
@@ -55,41 +46,5 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-  .card {
-    position: relative;
 
-    &>* {
-      z-index: 3;
-    }
-
-    &>img {
-      z-index: 1;
-      height: 100%;
-      position: absolute;
-      object-fit: cover;
-    }
-
-    &:before {
-      content: "";
-      display: block;
-      padding-top: 75%;
-    }
-
-    &:after {
-      content: '';
-      width: 100%;
-      height: 33.33%;
-      top: 0;
-      position: absolute;
-      background-color: rgba(0,0,0,.5);
-      z-index: 2;
-    }
-
-    .card-title {
-      white-space: nowrap;
-      overflow: hidden;
-      text-overflow: ellipsis;
-      font-size: medium;
-    }
-  }
 </style>
---
+++ src/components/LinkCard.vue
@@ -0,0 +1,69 @@
+<template>
+  <b-col sm="6" md="4" lg="3">
+    <router-link :to="link">
+      <b-card
+        overlay
+        :img-src="imagePath"
+        :img-alt="name"
+        text-variant="white"
+        :title="name"
+      ></b-card>
+    </router-link>
+  </b-col>
+</template>
+
+<script>
+  export default {
+    name: 'link-card',
+    props: {
+      link: String,
+      imagePath: String,
+      name: String
+    },
+    data () {
+      return {}
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+  .card {
+    position: relative;
+    margin-bottom: 10px;
+
+    &>* {
+      z-index: 3;
+    }
+
+    &>img {
+      z-index: 1;
+      height: 100%;
+      position: absolute;
+      object-fit: cover;
+    }
+
+    &:before {
+      content: "";
+      display: block;
+      padding-top: 75%;
+    }
+
+    &:after {
+      content: '';
+      width: 100%;
+      height: 33.33%;
+      top: 0;
+      position: absolute;
+      background-color: rgba(0,0,0,.5);
+      z-index: 2;
+    }
+
+    .card-title {
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      font-size: medium;
+    }
+  }
+</style>
